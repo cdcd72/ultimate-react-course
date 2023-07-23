cdf2e6cdf66cf675ee71fe35a3e514a372782f23
Neil <cdcd71517@gmail.com>
2023-07-23 21:30:26 +0800

    02-JS-review add starter dir

--- 02-JS-review/starter/script.js
+++ 02-JS-review/starter/script.js
@@ -142,3 +142,158 @@ function getBooks() {
 function getBook(id) {
   return data.find((d) => d.id === id);
 }
+
+const book = getBook(1);
+
+// Destructuring object
+const { title, author, genres, pages, publicationDate, hasMovieAdaptation } = book;
+
+console.log(title, '|', author);
+
+// Destructuring array & Rest Operator
+const [primaryGenre, secondaryGenre, ...otherGenres] = genres;
+
+console.log(genres);
+
+console.log(primaryGenre, '|', secondaryGenre, '|', otherGenres);
+
+// Spread Operator
+const newGenres = [...genres, 'epic fantasy'];
+
+console.log(newGenres);
+
+const updatedBook = {
+  ...book,
+  // Adding a new property
+  moviePublicationDate: '2001-12-19',
+  // Overwriting an existing property
+  pages: 1210
+};
+
+console.log(`${book.pages}`, '|', `${updatedBook.pages}`);
+
+// Arrow Functions
+const getYear = (date) => date.split('-')[0];
+
+// Template Literals
+const summary = `${title}, a ${pages}-page long book, was written by ${author} and published in ${getYear(publicationDate)}. The book has ${hasMovieAdaptation ? '' : 'not'} been adapted as a movie`;
+
+console.log(summary);
+
+// Ternaries
+const pagesRange = pages > 1000 ? 'over a thousand' : 'less than 1000';
+
+console.log(`The book has ${pagesRange} pages`);
+
+// &&
+console.log(true && "Some string");
+console.log(false && "Some string");
+console.log(hasMovieAdaptation && "This book has a movie");
+
+// falsy: 0, '', null, undefined
+console.log("jonas" && "Some string");
+console.log(0 && "Some string");
+
+// ||
+console.log(true || "Some string");
+console.log(false || "Some string");
+
+console.log(book.translations.spanish);
+
+const spanishTranslation = book.translations.spanish || "NOT TRANSLATED";
+console.log(spanishTranslation);
+
+console.log(book.reviews.librarything.reviewsCount);
+const countWrong = book.reviews.librarything.reviewsCount || "no data";
+console.log(countWrong);
+
+// ??
+const count = book.reviews.librarything.reviewsCount ?? "no data";
+count;
+
+function getTotalReviewCount(book) {
+  // Optional Chaining
+  const goodreads = book.reviews?.goodreads?.reviewsCount ?? 0;
+  const librarything = book.reviews?.librarything?.reviewsCount ?? 0;
+  return goodreads + librarything;
+}
+
+console.log(getTotalReviewCount(book));
+
+const books = getBooks();
+
+// Array map
+const x = [1, 2, 3, 4, 5].map((el) => el * 2);
+console.log(x);
+
+const titles = books.map((book) => book.title);
+console.log(titles);
+
+const essentialData = books.map((book) => ({
+  title: book.title,
+  author: book.author,
+  reviewsCount: getTotalReviewCount(book),
+}));
+console.log(essentialData);
+
+// Array filter
+const longBooksWithMovie = books
+  .filter((book) => book.pages > 500)
+  .filter((book) => book.hasMovieAdaptation);
+console.log(longBooksWithMovie);
+
+const adventureBooks = books
+  .filter((books) => books.genres.includes("adventure"))
+  .map((book) => book.title);
+console.log(adventureBooks);
+
+// Array reduce
+const pagesAllBooks = books.reduce((sum, book) => sum + book.pages, 0);
+console.log(pagesAllBooks);
+
+// Array sort
+const arr = [3, 7, 1, 9, 6];
+const sorted = arr.slice().sort((a, b) => a - b);
+console.log(sorted);
+console.log(arr);
+
+const sortedByPages = books.slice().sort((a, b) => a.pages - b.pages);
+console.log(sortedByPages);
+
+//#region Working With Immutable Arrays
+
+// 1) Add book object to array
+const newBook = {
+  id: 6,
+  title: "Harry Potter and the Chamber of Secrets",
+  author: "J. K. Rowling",
+};
+const booksAfterAdd = [...books, newBook];
+console.log(booksAfterAdd);
+
+// 2) Delete book object from array
+const booksAfterDelete = booksAfterAdd.filter((book) => book.id !== 3);
+console.log(booksAfterDelete);
+
+// 3) Update book object in the array
+const booksAfterUpdate = booksAfterDelete.map((book) =>
+  book.id === 1 ? { ...book, pages: 1210 } : book
+);
+console.log(booksAfterUpdate);
+
+//#endregion
+
+// Promises example
+// fetch("https://jsonplaceholder.typicode.com/todos")
+//   .then((res) => res.json())
+//   .then((data) => console.log(data));
+
+// Async/Await example
+async function getTodos() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/todos");
+  const data = await res.json();
+  return data;
+}
+
+const todos = await getTodos();
+console.log(todos);
