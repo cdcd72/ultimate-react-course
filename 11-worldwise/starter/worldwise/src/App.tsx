7b4c6dc5aec5068bc6f0077f374990aec7fce366
cdcd72 <cdcd71517@gmail.com>
2023-08-31 10:55:17 +0800

    11-worldwise dir updated 08/31 10:55

--- 11-worldwise/starter/worldwise/src/App.tsx
+++ 11-worldwise/starter/worldwise/src/App.tsx
@@ -1,5 +1,21 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import Home from './pages/Home';
+import Product from './pages/Product';
+import Pricing from './pages/Pricing';
+import NotFound from './pages/NotFound';
+
 function App() {
-  return <div>WorldWise</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="product" element={<Product />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
