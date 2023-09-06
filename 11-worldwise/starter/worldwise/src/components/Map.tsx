352aeea62292b30ecc0cab44dd06ec637d5cece0
cdcd72 <cdcd71517@gmail.com>
2023-09-06 11:44:12 +0800

    11-worldwise dir updated 09/06 11:44

--- 11-worldwise/starter/worldwise/src/components/Map.tsx
+++ 11-worldwise/starter/worldwise/src/components/Map.tsx
@@ -1,19 +1,55 @@
-import { useNavigate, useSearchParams } from 'react-router-dom';
 import styles from './Map.module.css';
 
+import { useEffect, useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
+import { MapContainer, Marker, Popup, TileLayer, useMap } from 'react-leaflet';
+import { LatLngExpression } from 'leaflet';
+
+import { useCities } from '../contexts/CitiesContext';
+
 function Map() {
-  const [searchParams, setSearchParams] = useSearchParams();
+  const { cities } = useCities();
+  const [mapPosition, setMapPosition] = useState({ lat: 40, lng: 0 });
+  const [searchParams] = useSearchParams();
   const lat = searchParams.get('lat');
   const lng = searchParams.get('lng');
-  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (lat && lng) {
+      setMapPosition({ lat: +lat, lng: +lng });
+    }
+  }, [lat, lng]);
+
   return (
-    <div className={styles.mapContainer} onClick={() => navigate('form')}>
-      <h1>Map</h1>
-      <h1>
-        Position: {lat}, {lng}
-      </h1>
+    <div className={styles.mapContainer}>
+      <MapContainer
+        className={styles.map}
+        center={mapPosition}
+        zoom={13}
+        scrollWheelZoom={true}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+        />
+        {cities.map((city) => (
+          <Marker position={city.position} key={city.id}>
+            <Popup>
+              <span>{city.emoji}</span>
+              <span>{city.cityName}</span>
+            </Popup>
+          </Marker>
+        ))}
+        <ChangeCenter position={mapPosition} />
+      </MapContainer>
     </div>
   );
+}
+
+function ChangeCenter({ position }: { position: LatLngExpression }) {
+  const map = useMap();
+  map.setView(position);
+  return null;
 }
 
 export default Map;
