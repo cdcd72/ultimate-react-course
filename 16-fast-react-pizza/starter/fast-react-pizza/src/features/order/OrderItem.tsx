cceb5e12d5d69483b019e87481612bb0e825a044
Neil <cdcd71517@gmail.com>
2023-10-19 18:53:22 +0800

    16-fast-react-pizza dir updated 10/19 18:53

--- 16-fast-react-pizza/starter/fast-react-pizza/src/features/order/OrderItem.tsx
+++ 16-fast-react-pizza/starter/fast-react-pizza/src/features/order/OrderItem.tsx
@@ -1,13 +1,24 @@
-function OrderItem({ item, isLoadingIngredients, ingredients }) {
+import { Cart } from '../../models/cart';
+import { formatCurrency } from '../../utils/helpers';
+
+function OrderItem({
+  item,
+  isLoadingIngredients,
+  ingredients,
+}: {
+  item: Cart;
+  isLoadingIngredients?: boolean;
+  ingredients?: string[];
+}) {
   const { quantity, name, totalPrice } = item;
 
   return (
-    <li>
-      <div>
+    <li className="py-3">
+      <div className="flex items-center justify-between gap-4 text-sm">
         <p>
-          <span>{quantity}&times;</span> {name}
+          <span className="font-bold">{quantity}&times;</span> {name}
         </p>
-        <p>{formatCurrency(totalPrice)}</p>
+        <p className="font-bold">{formatCurrency(totalPrice)}</p>
       </div>
     </li>
   );
