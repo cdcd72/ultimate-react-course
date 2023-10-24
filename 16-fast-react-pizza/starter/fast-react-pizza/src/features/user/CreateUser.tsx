412bdd855257a2be2ab4e459340a8b2128a49c49
cdcd72 <cdcd71517@gmail.com>
2023-10-24 12:07:23 +0800

    16-fast-react-pizza dir updated 10/24 12:07

--- 16-fast-react-pizza/starter/fast-react-pizza/src/features/user/CreateUser.tsx
+++ 16-fast-react-pizza/starter/fast-react-pizza/src/features/user/CreateUser.tsx
@@ -1,11 +1,19 @@
-import { useState } from 'react';
+import { FormEvent, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+
 import Button from '../../ui/Button';
+import { updateName } from './userSlice';
 
 function CreateUser() {
   const [username, setUsername] = useState('');
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-  function handleSubmit(e) {
+  function handleSubmit(e: FormEvent<HTMLFormElement>) {
     e.preventDefault();
+    dispatch(updateName(username));
+    navigate('/menu');
   }
 
   return (
