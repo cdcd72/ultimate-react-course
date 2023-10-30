42b94bddf208ff89afed0b0447ec697e194f9ac0
cdcd72 <cdcd71517@gmail.com>
2023-10-30 18:01:48 +0800

    17-the-wild-oasis dir updated 10/30 18:01

--- 17-the-wild-oasis/starter/the-wild-oasis/src/features/cabins/CreateCabinForm.tsx
+++ 17-the-wild-oasis/starter/the-wild-oasis/src/features/cabins/CreateCabinForm.tsx
@@ -48,7 +48,8 @@ const Error = styled.span`
 `;
 
 function CreateCabinForm() {
-  const { register, handleSubmit, reset } = useForm();
+  const { register, handleSubmit, reset, getValues, formState } = useForm();
+  const { errors } = formState;
   const queryClient = useQueryClient();
   const { isLoading: isCreating, mutate } = useMutation({
     mutationFn: createCabin,
@@ -74,21 +75,52 @@ function CreateCabinForm() {
     mutate(cabin);
   }
 
+  function onSubmitError(errors: FieldValues) {
+    console.log(errors);
+  }
+
   return (
-    <Form onSubmit={handleSubmit(onSubmit)}>
+    <Form onSubmit={handleSubmit(onSubmit, onSubmitError)}>
       <FormRow>
         <Label htmlFor="name">Cabin name</Label>
-        <Input type="text" id="name" {...register('name')} />
+        <Input
+          type="text"
+          id="name"
+          {...register('name', {
+            required: 'This field is required',
+          })}
+        />
+        {errors?.name?.message && <Error>{errors.name.message}</Error>}
       </FormRow>
 
       <FormRow>
         <Label htmlFor="maxCapacity">Maximum capacity</Label>
-        <Input type="number" id="maxCapacity" {...register('maxCapacity')} />
+        <Input
+          type="number"
+          id="maxCapacity"
+          {...register('maxCapacity', {
+            required: 'This field is required',
+            min: {
+              value: 1,
+              message: 'Capacity should be at least 1',
+            },
+          })}
+        />
       </FormRow>
 
       <FormRow>
         <Label htmlFor="regularPrice">Regular price</Label>
-        <Input type="number" id="regularPrice" {...register('regularPrice')} />
+        <Input
+          type="number"
+          id="regularPrice"
+          {...register('regularPrice', {
+            required: 'This field is required',
+            min: {
+              value: 1,
+              message: 'Regular price should be at least 1',
+            },
+          })}
+        />
       </FormRow>
 
       <FormRow>
@@ -97,7 +129,12 @@ function CreateCabinForm() {
           type="number"
           id="discount"
           defaultValue={0}
-          {...register('discount')}
+          {...register('discount', {
+            required: 'This field is required',
+            validate: (value) =>
+              Number(value) <= Number(getValues().regularPrice) ||
+              'Discount should be less than regular price',
+          })}
         />
       </FormRow>
 
@@ -107,7 +144,9 @@ function CreateCabinForm() {
           type="number"
           id="description"
           defaultValue=""
-          {...register('description')}
+          {...register('description', {
+            required: 'This field is required',
+          })}
         />
       </FormRow>
 
